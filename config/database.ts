2dbdcc87e0c9072e65152d7d44c4351ed11a571e
chandrasekar-velu23 <chandrasekarvelu23@gmail.com>
2026-06-26 11:04:45 +0530

    migrated to postgres

--- config/database.ts
+++ config/database.ts
@@ -24,27 +24,39 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Database 
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
     postgres: {
-      connection: env('DATABASE_URL')
-        ? {
-            connectionString: env('DATABASE_URL'),
-            ssl: {
-              rejectUnauthorized: false, 
-            },
-            schema: env('DATABASE_SCHEMA', 'public'),
-          }
-        : {
-            host: env('DATABASE_HOST', 'localhost'),
-            port: env.int('DATABASE_PORT', 5432),
-            database: env('DATABASE_NAME', 'strapi'),
-            user: env('DATABASE_USERNAME', 'strapi'),
-            password: env('DATABASE_PASSWORD', 'strapi'),
-            ssl: env.bool('DATABASE_SSL', false) && {
-              rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-            },
-            schema: env('DATABASE_SCHEMA', 'public'),
-          },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
+  connection: {
+    connectionString: env("DATABASE_URL"),
+    ssl: env.bool("DATABASE_SSL", true)
+      ? { rejectUnauthorized: false }
+      : false,
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+},
+    // postgres: {
+    //   connection: env('DATABASE_URL')
+    //     ? {
+    //         connectionString: env('DATABASE_URL'),
+    //         ssl: {
+    //           rejectUnauthorized: false, 
+    //         },
+    //         schema: env('DATABASE_SCHEMA', 'public'),
+    //       }
+    //     : {
+    //         host: env('DATABASE_HOST', 'localhost'),
+    //         port: env.int('DATABASE_PORT', 5432),
+    //         database: env('DATABASE_NAME', 'strapi'),
+    //         user: env('DATABASE_USERNAME', 'strapi'),
+    //         password: env('DATABASE_PASSWORD', 'strapi'),
+    //         ssl: env.bool('DATABASE_SSL', false) && {
+    //           rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+    //         },
+    //         schema: env('DATABASE_SCHEMA', 'public'),
+    //       },
+    //   pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+    // },
     sqlite: {
       connection: {
         filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
